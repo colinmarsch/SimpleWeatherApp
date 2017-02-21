4c225d2534f21375b5bb8a42a13dbc157fa14044
Colin Marsch <colinmarsch@gmail.com>
2017-02-21 17:31:27 -0600

    Added support for getting the current locations weather

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 * Find a better way to get a weather icon so it isn't blurry
 * Implement functioning autocomplete on the city search activity
 * Add another view for future forecasts (5 or 7 days)
-* Get weather for your current location
+* Fix name of current location showing up as "City Centre"
 * UI upgrades
--- app/src/main/java/me/colinmarsch/simpleweather/simpleweatherapp/MainActivity.java
+++ app/src/main/java/me/colinmarsch/simpleweather/simpleweatherapp/MainActivity.java
@@ -1,8 +1,15 @@
 package me.colinmarsch.simpleweather.simpleweatherapp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,24 +21,36 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public TextView mTxtTemperature, mTxtDetails, mChange, mCityName;
     public ImageView mImgView;
+    public GoogleApiClient mApiClient;
+    public Location mCurrLocation;
     Weather helper = Weather.getInstance();
     private final static String API_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather?units=metric";
-    private final static String APIKEY = "0f9cfc3727985ab2180dc4cbe36b3446";
+    private final static String APIKEY = "API_KEY";
     private String city = "Waterloo,ON";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(mApiClient == null) {
+            mApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
         mTxtDetails = (TextView) findViewById(R.id.details);
         mImgView = (ImageView) findViewById(R.id.main_bg);
         mCityName = (TextView) findViewById(R.id.city_name);
@@ -40,7 +59,19 @@ public class MainActivity extends AppCompatActivity {
         final Button button = (Button) findViewById(R.id.refresh);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                loadData();
+                loadData(API_ENDPOINT + "&q=" + city + "&appid=" + APIKEY);
+            }
+        });
+        final Button loc_button = (Button) findViewById(R.id.getLocation);
+        loc_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                    onConnected(new Bundle());
+                } else {
+                    loadData(API_ENDPOINT + "&lat=" + mCurrLocation.getLatitude() +
+                            "&lon=" + mCurrLocation.getLongitude() + "&appid=" + APIKEY);
+                }
             }
         });
         mChange.setOnClickListener(new View.OnClickListener() {
@@ -49,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, 2);
             }
         });
-        loadData();
+        loadData(API_ENDPOINT + "&q=" + city + "&appid=" + APIKEY);
     }
 
 
@@ -73,16 +104,14 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     }
-                    loadData();
+                    loadData(API_ENDPOINT + "&q=" + city + "&appid=" + APIKEY);
                 }
                 break;
             }
         }
     }
 
-
-    private void loadData() {
-        String url = API_ENDPOINT + "&q=" + city + "&appid=" + APIKEY;
+    private void loadData(String url) {
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
@@ -94,8 +123,8 @@ public class MainActivity extends AppCompatActivity {
                             int imgRes = getResources().getIdentifier(uri, null, getPackageName());
                             Drawable res = getResources().getDrawable(imgRes, null);
                             mImgView.setImageDrawable(res);
-                            mCityName.setText(response.getString("name") + ", "
-                                    + response.getJSONObject("sys").getString("country"));
+                            city = response.getString("name") + ", " + response.getJSONObject("sys").getString("country");
+                            mCityName.setText(city);
                             mTxtTemperature.setText(response.getJSONObject("main").getString("temp") + "°C");
                             mTxtDetails.setText(response.getJSONArray("weather").getJSONObject(0).getString("description"));
 
@@ -111,5 +140,32 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         helper.add(jsObjRequest);
+    }
+
+    protected void onStart() {
+        mApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mCurrLocation = LocationServices.FusedLocationApi.getLastLocation(mApiClient);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
