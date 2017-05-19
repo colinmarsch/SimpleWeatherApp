900e14fd89ec3e5d891ebda5c1adb75ff1d2bcfa
Colin Marsch <colinmarsch@gmail.com>
2017-05-18 22:39:38 -0400

    Added icons on the saved cities list

--- app/src/main/java/me/colinmarsch/simpleweather/simpleweatherapp/CityListAdapter.java
+++ app/src/main/java/me/colinmarsch/simpleweather/simpleweatherapp/CityListAdapter.java
@@ -1,10 +1,12 @@
 package me.colinmarsch.simpleweather.simpleweatherapp;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -16,11 +18,15 @@ import org.w3c.dom.Text;
 public class CityListAdapter extends ArrayAdapter<String> {
 
     private String[] cities;
+    private String[] imgID;
+    private String[] temps;
     private Activity context;
 
-    public CityListAdapter(Activity context, String[] cities) {
+    public CityListAdapter(Activity context, String[] cities, String[] imgID, String[] temps) {
         super(context, R.layout.cities_list, cities);
         this.cities = cities;
+        this.imgID = imgID;
+        this.temps = temps;
         this.context = context;
     }
 
@@ -30,6 +36,15 @@ public class CityListAdapter extends ArrayAdapter<String> {
 
         TextView city_name = (TextView) rowView.findViewById(R.id.city_name);
         city_name.setText(cities[position]);
+
+        int imgRes = context.getResources().getIdentifier(imgID[position], null, getContext().getPackageName());
+        Drawable res = context.getResources().getDrawable(imgRes, null);
+        ImageView mIcon = (ImageView) rowView.findViewById(R.id.current_city_icon);
+        mIcon.setImageDrawable(res);
+
+        TextView currTemp = (TextView) rowView.findViewById(R.id.temperature);
+        currTemp.setText(temps[position]);
+
         return rowView;
     }
 }
--- app/src/main/java/me/colinmarsch/simpleweather/simpleweatherapp/SavedCities.java
+++ app/src/main/java/me/colinmarsch/simpleweather/simpleweatherapp/SavedCities.java
@@ -16,11 +16,22 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import me.colinmarsch.simpleweather.simpleweatherapp.data.SavedCitiesContract.CitiesEntry;
 import me.colinmarsch.simpleweather.simpleweatherapp.data.SavedCitiesHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.data;
+import static me.colinmarsch.simpleweather.simpleweatherapp.R.string.error;
 
 /**
  * Created by colinmarsch on 2017-05-01.
@@ -34,6 +45,12 @@ public class SavedCities extends Fragment {
     private ContentValues values;
     private SQLiteDatabase db;
     private ListView list;
+    private String[] imgID;
+    private String[] temps;
+    private View view;
+    private final static String API_ENDPOINT = "http://api.openweathermap.org/data/2.5/weather?units=metric";
+    private final static String APIKEY = "0f9cfc3727985ab2180dc4cbe36b3446";
+    Weather helper = Weather.getInstance();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,7 +58,7 @@ public class SavedCities extends Fragment {
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.cities_layout, container, false);
+        view = inflater.inflate(R.layout.cities_layout, container, false);
         citiesList = new ArrayList<String>();
         cities = citiesList.toArray(new String[citiesList.size()]);
         add_city = (Button) view.findViewById(R.id.add_city);
@@ -78,10 +95,38 @@ public class SavedCities extends Fragment {
         while(c.moveToNext()) {
             citiesList.add(c.getString(columnIndex));
         }
+        imgID = new String[citiesList.size()];
+        temps = new String[citiesList.size()];
+        for(String city : citiesList) {
+            final int i = citiesList.indexOf(city);
+            System.out.println("this ran here");
+            String url = API_ENDPOINT + "&q=" + city + "&appid=" + APIKEY;
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                String uri = "@drawable/a"
+                                        + response.getJSONArray("weather").getJSONObject(0).getString("icon");
+                                imgID[i] = uri;
+                                String temp = response.getJSONObject("main").getString("temp") + "°C";
+                                temps[i] = temp;
+                                CityListAdapter adapter= new CityListAdapter(getActivity(), cities, imgID, temps);
+                                list.setAdapter(adapter);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    });
+            helper.add(jsonObjectRequest);
+        }
         cities = citiesList.toArray(new String[citiesList.size()]);
-        CityListAdapter adapter= new CityListAdapter(getActivity(), cities);
         list = (ListView) view.findViewById(R.id.cities_listView);
-        list.setAdapter(adapter);
         c.close();
     }
 
